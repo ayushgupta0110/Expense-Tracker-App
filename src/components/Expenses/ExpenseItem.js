dc0a675f77f6002f9ef4c003f7891dc2bb8bcca8
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-05-20 22:43:20 +0530

    Updated the function syntax to the arrow one

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,7 @@ import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem(props){
+const ExpenseItem = (props) => {
 
     return (
         <Card className="expense-item">
