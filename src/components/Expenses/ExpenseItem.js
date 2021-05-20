806b4a61d00d9f7ba932b8a59bf3340fb5f24d01
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-05-20 23:58:44 +0530

    Added useState hook to the title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import './ExpenseItem.css';
-import Card from '../UI/Card';
-import ExpenseDate from './ExpenseDate';
+import React, { useState } from "react";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
 
-    return (
-        <Card className="expense-item">
-          <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-        </Card>
-    );
-}
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
