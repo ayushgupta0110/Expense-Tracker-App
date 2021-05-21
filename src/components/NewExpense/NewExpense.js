b351816f5c0a744511a183a15b2af8e99ab4cf4b
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-05-21 17:32:27 +0530

    Added a new sub-folder in the compnents name NewExpense

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewExpense.css';
+
+const NewExpense = () => {
+    return (
+        <div className="new-expense">
+            <form action="">
+
+            </form>
+        </div>
+    )
+}
+
+export default NewExpense
