dd180731f642981215ec158b52421ec4af728595
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-05-21 18:27:02 +0530

    using One state instead of Multiple state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,22 +2,35 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const [enteredTitle,setEnteredTitle] = useState('');
-    const [enteredAmount,setEnteredAmount] = useState(0);
-    const [enteredDate,setEnteredDate] = useState();
+//   const [enteredTitle, setEnteredTitle] = useState("");
+//   const [enteredAmount, setEnteredAmount] = useState("");
+//   const [enteredDate, setEnteredDate] = useState("");
+    const [userInput,setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: '',
+    })
 
-    const titleChangeHandler = (event) =>{
-        setEnteredTitle(event.target.value);
-    }
+  const titleChangeHandler = (event) => {
+    setUserInput({
+        ...userInput,
+        enteredTitle: event.target.value,
+    });
+  };
 
-    const amountChangeHandler = event =>{
-        setEnteredAmount(event.target.value);
-    };
+  const amountChangeHandler = (event) => {
+    setUserInput({
+        ...userInput,
+        enteredAmount: event.target.value,
+    });
+ };
 
-    const dateChangeHandler = event =>{
-        setEnteredDate(event.target.value);
-    };
-
+  const dateChangeHandler = (event) => {
+    setUserInput({
+        ...userInput,
+        enteredDate: event.target.value,
+    });
+ };
 
   return (
     <form>
@@ -28,11 +41,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
