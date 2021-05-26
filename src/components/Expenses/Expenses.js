b683b9d9f06961cbb25abfdf3afce212d034f2e5
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-05-27 00:18:56 +0530

    Added new component ExpensesList.js to implement cond return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2021");
@@ -11,32 +12,22 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpense = props.items.filter(expense => {
+  const filteredExpense = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>
-
-  if(filteredExpense.length >0){
-    expensesContent= filteredExpense.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        date={expense.date}
-        amount={expense.amount}
-      />
-    ))
-  }
   return (
-    <div>
-      <Card className="expenses">
-        <ExpenseFilter
-          selectedYear={filteredYear}
-          onChangeFilter={onChangeFilterHandler}
-        />
-        {expensesContent}
-      </Card>
-    </div>
+    <li>
+      <div>
+        <Card className="expenses">
+          <ExpenseFilter
+            selectedYear={filteredYear}
+            onChangeFilter={onChangeFilterHandler}
+          />
+          <ExpensesList items={filteredExpense} />
+        </Card>
+      </div>
+    </li>
   );
 };
 
